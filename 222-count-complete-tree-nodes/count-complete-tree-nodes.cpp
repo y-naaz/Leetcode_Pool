29fd2ded9e5d450de9107d66cd03ad6b30009eb2
YASMEEN NAAZ <118757913+y-naaz@users.noreply.github.com>
2024-12-01 16:50:09 +0530

    Time: 0 ms (100.00%) | Memory: 31.3 MB (5.49%) - LeetSync

--- 222-count-complete-tree-nodes/count-complete-tree-nodes.cpp
+++ 222-count-complete-tree-nodes/count-complete-tree-nodes.cpp
@@ -11,17 +11,31 @@
  */
 class Solution {
 public:
-    void solve(TreeNode* root, int&cnt){
-        if(!root) return ;
-        cnt++;
-        solve(root->left,cnt);
-        solve(root->right,cnt);
-
+    int leftHeight(TreeNode* root){
+        if(!root) return 0 ; 
+        int lh = 0 ; 
+        while(root){
+            lh++;
+            root= root->left;
+        }
+        return lh ;
+    }
+    int rightHeight(TreeNode*root){
+        if(!root) return 0 ; 
+        int rh = 0 ; 
+        while(root){
+            rh++;
+            root= root->right;
+        }
+        return rh;
     }
     int countNodes(TreeNode* root) {
-        int cnt = 0 ; 
-        solve(root,cnt);
-        return cnt;
-
+        //method 2 :
+        //time complexity O(logN*logN)
+        if(!root) return 0 ; 
+        int lh = leftHeight(root);
+        int rh = rightHeight(root);
+        if(lh==rh) return (1<<lh)-1;
+        return 1 +countNodes(root->left) +countNodes(root->right);
     }
 };
