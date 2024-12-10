1b0379d0c2ff2ae4acc9fc71bd9b60819e606139
YASMEEN NAAZ <118757913+y-naaz@users.noreply.github.com>
2024-12-10 16:01:28 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (5.97%) - LeetSync

---
+++ 3379-score-of-a-string/score-of-a-string.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int scoreOfString(string s) {
+        int n = s.size();
+        int score = 0 ;
+        for(int i  = 0 ; i <n-1;i++){
+            score+=abs(s[i]-s[i+1]);
+        }
+        return score;
+    }
+};
